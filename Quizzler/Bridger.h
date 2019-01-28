0cf5e0065295cdbd5b135ef5212afd614cdcc7ff
VigneshRaghav <vigneshraghav@wifi-roaming-128-4-157-106.host.udel.edu>
2019-01-28 18:38:55 -0500

    first commit

---
+++ Quizzler/Bridger.h
@@ -0,0 +1,9 @@
+//
+//  Bridger.h
+//  Quizzler
+//
+//  Created by VigneshRaghav on 1/24/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+#import "ProgressHUD.h"
---
+++ Quizzler/Quizzler-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Quizzler-Bridging-Header.h
+//  Quizzler
+//
+//  Created by VigneshRaghav on 1/24/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+#ifndef Quizzler_Bridging_Header_h
+#define Quizzler_Bridging_Header_h
+
+
+#endif /* Quizzler_Bridging_Header_h */
